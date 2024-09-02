44459b470d6c2298a61c1b32bd78f62b6e99c1f8
fadlurahmanfdev <taufikfadlurahmanfajari.dev@gmail.com>
2024-09-02 10:34:08 +0700

    feature(docs): update docs

--- kotlin_feature_network/src/main/java/co/id/fadlurahmanfdev/kotlin_feature_network/data/repository/FeatureNetworkRepositoryImpl.kt
+++ kotlin_feature_network/src/main/java/co/id/fadlurahmanfdev/kotlin_feature_network/data/repository/FeatureNetworkRepositoryImpl.kt
@@ -12,6 +12,9 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class FeatureNetworkRepositoryImpl : FeatureNetworkRepository {
+    /**
+     * Creates a ChuckerInterceptor.Builder configured with the provided Context. This interceptor is used for inspecting and debugging HTTP requests and responses within the application.
+     */
     override fun getChuckerInterceptorBuilder(context: Context): ChuckerInterceptor.Builder {
         val chuckerCollector = ChuckerCollector(
             context = context,
@@ -26,10 +29,18 @@ class FeatureNetworkRepositoryImpl : FeatureNetworkRepository {
             .createShortcut(false)
     }
 
+    /**
+     * Generates a CertificatePinner.Builder, which is used to build a certificate pinner. This pinner ensures that only specified SSL certificates are accepted for secure connections, enhancing the security of network communications.
+     */
     override fun getCertificatePinnerBuilder(): CertificatePinner.Builder {
         return CertificatePinner.Builder()
     }
 
+    /**
+     * The getOkHttpClientBuilder function provides a way to configure an OkHttpClient with optional logging and SSL certificate pinning. Adjusting these parameters customizes the client's behavior according to specific needs.
+     * @param useLoggingInterceptor Indicates whether to include a logging interceptor in the OkHttpClient. When set to true, network requests and responses will be logged, which is useful for debugging.
+     * @param sslCertificatePinner Allows the addition of an SSL certificate pinner to the OkHttpClient for extra security by pinning specific certificates. If set to null, no certificate pinning is applied.
+     */
     override fun getOkHttpClientBuilder(
         useLoggingInterceptor: Boolean,
         sslCertificatePinner: CertificatePinner?
@@ -38,6 +49,7 @@ class FeatureNetworkRepositoryImpl : FeatureNetworkRepository {
             if (sslCertificatePinner != null) {
                 certificatePinner(sslCertificatePinner)
             }
+
             if (useLoggingInterceptor) {
                 addInterceptor(
                     HttpLoggingInterceptor().apply {
@@ -49,7 +61,14 @@ class FeatureNetworkRepositoryImpl : FeatureNetworkRepository {
     }
 
     /**
-     * @param callAdapterFactory put RxJava3CallAdapterFactory.create()
+     * The createAPI function efficiently sets up an API service.
+     * By providing the base URL, network connection settings, response format, and API interface, a fully functional service for handling network requests is created.
+     * @param baseUrl Specifies the main URL for the API. All requests will be directed to this address.
+     * For example, setting it to "https://api.example.com/" means that any requests made will begin with this URL.
+     * @param okHttpClient Handles the network connection. Customization options include adding timeouts or logging network calls.
+     * @param callAdapterFactory Determines how API responses are returned, such as in a simple object format or as an RxJava Observable.
+     * For example, setting it to RxJava3CallAdapterFactory.create().
+     * @param clazz Defines the API interface, specifying the various endpoints (actions) that can be performed within the code.
      * */
     override fun <T> createAPI(
         baseUrl: String,
